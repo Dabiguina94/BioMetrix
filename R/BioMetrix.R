b3af26083a84fe72f68c520f6cddc8f357f64043
Dabiguina94 <dabiguina@gmail.com>
2024-10-30 16:57:13 -0600

    Initial commit

---
+++ R/BioMetrix.R
@@ -0,0 +1,17 @@
+#' BioMetrix
+#'@description
+#'A partir de una matriz de datos, calcula la media.
+#'
+#' @param matrix, numeric
+#'
+#' @return matrix, mean
+#' @export
+#'
+#' @examples
+#' matrix <- matrix(1:9, nrow = 3, ncol = 3)
+#' bio_mean(matrix)
+#'
+bio_mean <- function(matrix) {
+    x <- mean(matrix)
+    return(x)
+}
